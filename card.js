e53f1cea11190ff5c1aac6c0987cc8435376d7b0
nomayen <nomayen.ohin@gmail.com>
2023-10-17 20:18:08 +0600

    local storage

--- card.js
+++ card.js
@@ -10,7 +10,7 @@ const addProduct = () =>{
 
 
     console.log(product, quantity);
-    displayProduct(product, quantity)
+    displayProduct(product, quantity);saveProductToLocalStorage(product, quantity)
 }
 
 
@@ -20,3 +20,38 @@ const displayProduct = (product,quantity ) =>{
    li.innerText = `${product} : ${quantity}`
    productContainer.appendChild(li)
 }
+
+const getStoredShoppingCard = () =>{
+    let cart = {}
+    const storedCard = localStorage.getItem('cart');
+    if (storedCard) {
+        cart = JSON.parse(storedCard)
+    }
+    return cart;
+}
+
+const saveProductToLocalStorage = (product, quantity) =>{
+
+const cart = getStoredShoppingCard()
+cart[product] = quantity;
+// console.log(cart);
+const cartStringify = JSON.stringify(cart);
+localStorage.setItem('cart', cartStringify)
+
+
+console.log(cartStringify);
+
+
+}
+
+const displayProductsFromLocalStorage = () =>{
+    const savedCart = getStoredShoppingCard();
+    for (const product in savedCart) {
+        const quantity = savedCart[product];
+        displayProduct(product,quantity)
+        console.log(product, quantity);
+    }
+    console.log(savedCart);
+}
+
+displayProductsFromLocalStorage()
